6a5bca20c597d579cc9abced5ee0bf7134e43d4f
Lucas Acuña <lucasnahuelnet@gmail.com>
2021-01-14 16:20:05 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        Posts
+      </div>
+    </Router>
+  );
+}
+
+export default App;
