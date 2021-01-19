c59dfd4852e2bfe364aed68099ea2122b42786bb
Lucas Acuña <lucasnahuelnet@gmail.com>
2021-01-19 16:39:55 -0300

    User Profile Data

--- src/components/posts/CreatePost.js
+++ src/components/posts/CreatePost.js
@@ -1,51 +1,54 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import { createPost } from '../../store/actions/postActions'
 import { Redirect } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
-class CreatePost extends Component {
-    state = {
-        title: '',
-        content: '',
+const CreatePost = ({ auth, createPost }) =>  {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const history = useHistory();
+
+    const handleChangeTitle = (e) => {
+        setTitle(e.target.value)
     }
 
-    handleChange = (e) => {
-        this.setState({
-            [e.target.id]: e.target.value
-        })
+    const handleChangeContent = (e) => {
+        setContent(e.target.value)
     }
     
-    handleSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
-        //console.log(this.state)
-        this.props.createPost(this.state)
+        const newPost = {
+            title,
+            content
+        }
+        createPost(newPost);
+        history.push('/')
     }
 
-    render() {
-        const { auth } = this.props;
+    if(!auth.uid) return <Redirect to="/signin" />
 
-        if(!auth.uid) return <Redirect to="/signin" />
-
-        return (
-            <div className="container">
-                <form onSubmit={this.handleSubmit} className="white card form">
-                    <h5 className="grey-text text-darken-3">Create Post</h5>
-                    <div className="input-field">
-                        <label htmlFor="title">Title</label>
-                        <input type="text" id="title" onChange={this.handleChange} />
-                    </div>
-                    <div className="input-field">
-                        <label htmlFor="content">Post Content</label>
-                        <textarea id="content" className="materialize-textarea" onChange={this.handleChange} />
-                    </div>
-                    <div className="input-field">
-                        <button className="btn cyan">Create Post</button>
-                    </div>
-                </form> 
-            </div>
-        )
-    }
+    return (
+        <div className="container">
+            <form onSubmit={handleSubmit} className="white card form">
+                <h5 className="grey-text text-darken-3">Create Post</h5>
+                <div className="input-field">
+                    <label htmlFor="title">Title</label>
+                    <input type="text" id="title" onChange={handleChangeTitle} />
+                </div>
+                <div className="input-field">
+                    <label htmlFor="content">Post Content</label>
+                    <textarea id="content" className="materialize-textarea" onChange={handleChangeContent} />
+                </div>
+                <div className="input-field">
+                    <button className="btn cyan">Create Post</button>
+                </div>
+            </form> 
+        </div>
+    )
 }
+
 
 const mapStateToProps = (state) => {
     return {
--- src/store/actions/postActions.js
+++ src/store/actions/postActions.js
@@ -2,10 +2,14 @@ export const createPost = (post) => {
   return (dispatch, getState, { getFirebase, getFirestore}) => {
       //make async call to database
       const firestore = getFirestore();
+      const profile = getState().firebase.profile;
+      const authorId = getState().firebase.auth.uid;
+
       firestore.collection('posts').add({
         ...post,
-        authorFirstName: 'Franco',
-        authorLastName: 'Podio',
+        authorFirstName: profile.firstName,
+        authorLastName: profile.lastName,
+        authorId: authorId,
         createdAt: new Date()
       }).then(() => {
         dispatch({ type: 'ADD_POST', post: post})
